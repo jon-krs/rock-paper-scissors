4c233e1a6d74e60f8a7dae055ede3cb77e337862
jon-krs <jonkrs53@gmail.com>
2022-08-29 18:23:18 -0700

    Add comments to functions

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// Generates random integer between 0 to 2. 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
+// Uses number from getRandomInt to generate computer choice for game.
 function getComputerChoice() {
     let randomInt = getRandomInt(3);
     let computerChoice;
@@ -17,6 +19,9 @@ function getComputerChoice() {
     return computerChoice;
 }
 
+// Prompts player to choose rock, paper, or scissors for game.
+// Will continue to prompt if anything other than case-insensitive 'rock',
+// 'paper', or 'scissors' is typed.
 function getPlayerChoice() {
     let playerChoice = prompt('Choose your weapon!');
     playerChoice = playerChoice.toLowerCase();
@@ -32,6 +37,8 @@ function getPlayerChoice() {
     return playerChoice;
 }
 
+// Plays round of rock, paper, scissors. 
+// Returns a number between 0 to 2 to signal who won.
 function playRound(playerChoice, computerChoice) {
     let userWins = `You win a round! ${playerChoice} beats ${computerChoice}.`;
     let computerWins = `You lose a round! ${computerChoice} beats ${playerChoice}.`;
@@ -81,6 +88,8 @@ function playRound(playerChoice, computerChoice) {
     }
 }
 
+// Plays 5 rounds of rock, paper, scissors. 
+// Returns winner, loser, or tie.
 function game() {
     let userScore = 0;
     let computerScore = 0;
